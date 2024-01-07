0f1450462239a9f6ea531bea48371825721cf78c
Ahmadalhindi <>
2024-01-07 20:15:55 +0000

    Link ALLOWED_HOSTS to Heroku app to complete deployment.

--- lithium/settings.py
+++ lithium/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['lithium2023.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['lithium2023-734017e000a2.herokuapp.com']
 
 
 # Application definition
