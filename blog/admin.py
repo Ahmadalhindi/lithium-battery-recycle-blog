6636a319cd1cec13f0996bd9936686d291627b08
Ahmadalhindi <>
2024-01-10 16:33:01 +0000

    Continue building the admin site by adding register Category and Comment class in admin.py with attributes inherited from Summernote.

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,29 @@
 from django.contrib import admin
-from .models import Post
+from .models import Category, Post, Comment
 from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(SummernoteModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+
+
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_at')
     summernote_fields = ('content',)
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('name', 'body', 'post', 'created_at', 'approved')
+    list_filter = ('approved', 'created_at')
+    search_fields = ('name', 'email', 'body')
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
