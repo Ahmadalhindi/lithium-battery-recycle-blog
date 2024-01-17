c1dd3e6b81cdddaeee7d2f6f03190f88b78047f9
Ahmadalhindi <>
2024-01-17 18:06:23 +0000

    Build the Copyright and link to the home top of the page, change the name of hompe page in view to home.

--- blog/urls.py
+++ blog/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import PostListView
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='post_list'),
+    path('', PostListView.as_view(), name='home'),
     path('<str:category_name>/', PostListView.as_view(), name='post_list_by_category'),
 ]
