723ced2c2c896cd8d1de3da62fb063eddca24cfd
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-01-28 08:37:39 +0000

    Add docstrings and comments to the classes and functions in views python file.

--- blog/views.py
+++ blog/views.py
@@ -6,12 +6,31 @@ from .forms import CommentForm
 
 
 class PostListView(generic.ListView):
+    """
+    View for displaying a list of posts, optionally filtered by category.
+
+    Attributes:
+        model (Post): The model to use for the queryset.
+        queryset (QuerySet): The queryset of posts to be displayed.
+        template_name (str): The name of the template to render.
+        paginate_by (int): Number of posts per page for pagination.
+
+    Methods:
+        get_queryset: Get the queryset of posts based on the category or all posts.
+        get_context_data: Get additional context data to be used in the template.
+    """
     model = Post
     queryset = Post.objects.filter(status=1).order_by("created_at")
     template_name = 'post_list.html'
     paginate_by = 4
 
     def get_queryset(self):
+        """
+        Get the queryset for the post list view, optionally filtered by category.
+
+        Returns:
+            QuerySet: Filtered queryset of posts.
+        """
         category_name = self.kwargs.get('category_name', None)
         if category_name:
             return Post.objects.filter(category__name=category_name)
@@ -19,14 +38,37 @@ class PostListView(generic.ListView):
             return Post.objects.all()
 
     def get_context_data(self, **kwargs):
+        """
+        Get additional context data for the post list view, including all categories.
+
+        Returns:
+            dict: Additional context data.
+        """
         context = super().get_context_data(**kwargs)
         context['categories'] = Category.objects.all()
         return context
 
 
 class PostDetail(generic.View):
+    """
+    View for displaying the details of a single post, including comments and like status.
+
+    Methods:
+        get: Handle GET requests for viewing a post.
+        post: Handle POST requests for submitting comments on a post.
+    """
 
     def get(self, request, slug, *args, **kwargs):
+        """
+        Handle GET requests for viewing a post.
+
+        Args:
+            request: The HTTP request.
+            slug (str): The slug of the post.
+
+        Returns:
+            HttpResponse: Rendered template with post details.
+        """
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         category = post.category
@@ -48,7 +90,16 @@ class PostDetail(generic.View):
         )
     
     def post(self, request, slug, *args, **kwargs):
+        """
+        Handles POST requests for post detail view, including comment submission.
 
+        Args:
+            request: The HTTP request.
+            slug (str): The slug of the post.
+
+        Returns:
+            HttpResponse: Rendered template with updated post details.
+        """
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         category = post.category
@@ -82,8 +133,24 @@ class PostDetail(generic.View):
 
 
 class PostLike(View):
+    """
+    View for handling post likes.
+
+    Methods:
+        post: Handle POST requests for liking/unliking a post.
+    """
     
     def post(self, request, slug, *args, **kwargs):
+        """
+        Handle POST requests for liking/unliking a post.
+
+        Args:
+            request: The HTTP request.
+            slug (str): The slug of the post.
+
+        Returns:
+            HttpResponseRedirect: Redirect to the post detail page after liking/unliking.
+        """
         post = get_object_or_404(Post, slug=slug)
         if post.likes.filter(id=request.user.id).exists():
             post.likes.remove(request.user)
