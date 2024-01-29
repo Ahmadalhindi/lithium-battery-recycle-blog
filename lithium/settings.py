7bb38ffa40931cec8c9035dedbc50e2108bba027
Ahmad alhindi <ahmadyoussiv@gmail.com>
2024-01-29 20:28:10 +0000

    Change allowed hosts for deployment.

--- lithium/settings.py
+++ lithium/settings.py
@@ -35,7 +35,7 @@ DEBUG = True
 X_FRAME_OPTIONS = 'SAMEORIGIN'
 
 
-ALLOWED_HOSTS = ['8000-ahmadalhind-lithiumbatt-4ws8a93a5da.ws-eu107.gitpod.io']
+ALLOWED_HOSTS = ['lithium2023-734017e000a2.herokuapp.com']
 
 
 # Application definition
